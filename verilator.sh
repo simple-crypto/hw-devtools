ea38e494d43918d0730d4393e23dd6d0b8d9ce39
Gaëtan Cassiers <gaetan.cassiers@uclouvain.be>
2024-10-02 10:27:21 +0200

    Fix verilator build (clean after deps)

--- verilator.sh
+++ verilator.sh
@@ -8,4 +8,6 @@ git clone https://salsa.debian.org/electronics-team/verilator
 cd verilator
 git checkout debian/${VERILATOR_VERSION}-1
 sudo mk-build-deps -i debian/control
+git reset --hard
+git clean -f -d
 gbp buildpackage --git-ignore-branch --no-sign
