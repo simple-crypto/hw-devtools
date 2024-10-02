ffb81995e77793c7525273702599c674853aa03c
Gaëtan Cassiers <gaetan.cassiers@uclouvain.be>
2024-10-02 14:44:42 +0200

    Revert broken change in verilator debian package

--- verilator.sh
+++ verilator.sh
@@ -7,6 +7,8 @@ sudo apt install git-buildpackage equivs
 git clone https://salsa.debian.org/electronics-team/verilator
 cd verilator
 git checkout debian/${VERILATOR_VERSION}-1
+git checkout -b simple-dev
+git revert --no-edit cd7e5355a0e14953961a16a97cd8541f08acf370
 sudo mk-build-deps -i debian/control
 git reset --hard
 git clean -f -d
