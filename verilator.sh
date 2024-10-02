442f022834087f1060123f2417eca6dc3c99d321
Gaëtan Cassiers <gaetan.cassiers@uclouvain.be>
2024-10-02 16:04:40 +0200

    Fix broken revert in CI

--- verilator.sh
+++ verilator.sh
@@ -2,12 +2,14 @@
 
 VERILATOR_VERSION=5.024
 
-sudo apt install git-buildpackage equivs
+sudo apt-get -y install git-buildpackage equivs
 
 git clone https://salsa.debian.org/electronics-team/verilator
 cd verilator
 git checkout debian/${VERILATOR_VERSION}-1
 git checkout -b simple-dev
+git config author.name CI
+git config author.email ci@local
 git revert --no-edit cd7e5355a0e14953961a16a97cd8541f08acf370
 sudo mk-build-deps -i debian/control
 git reset --hard
