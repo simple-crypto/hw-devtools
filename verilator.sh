7705a06d38d30947de262a730a660c1914307ffb
cmomin <charles.momin@uclouvain.be>
2024-10-02 17:57:04 +0200

    Rollback to verilator v5.006

--- verilator.sh
+++ verilator.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-VERILATOR_VERSION=5.024
+VERILATOR_VERSION=5.006
 
 sudo apt-get -y install git-buildpackage equivs
 
 git clone https://salsa.debian.org/electronics-team/verilator
 cd verilator
-git checkout debian/${VERILATOR_VERSION}-1
+git checkout debian/${VERILATOR_VERSION}-2
 git checkout -b simple-dev
 git config user.name CI
 git config user.email ci@local
-git revert --no-edit cd7e5355a0e14953961a16a97cd8541f08acf370
+#git revert --no-edit cd7e5355a0e14953961a16a97cd8541f08acf370
 sudo mk-build-deps -i debian/control
 git reset --hard
 git clean -f -d
